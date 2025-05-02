60026f0f4ef3d1d0565a2a5d0f38aa85d5640227
shubhi-lab <shubhishrivastavajp3108@gmail.com>
2025-05-02 22:48:22 +0530

    added three new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,16 @@
 print("adding all ML related code here")
+
+def model1():
+    print("adding model1 code here")
+    # Code for model1
+    pass
+
+def model2():
+    print("adding model2 code here")
+    # Code for model2
+    pass
+
+def model3():
+    print("adding model3 code here")
+    # Code for model3
+    pass
